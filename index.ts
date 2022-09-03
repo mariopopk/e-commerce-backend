085959bf005a39a3a7402acf47fac313e7ce7347
mariopopk <travesura-realizada@hotmail.com>
2022-09-02 21:32:45 -0600

    Working minimal ts server

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { ApolloServer } from "apollo-server";
+import { readFileSync } from "fs";
+import { resolvers } from "./src/resolvers";
+import path from "path";
+
+const typeDefs = readFileSync(
+    path.join(__dirname, "./src/schema.graphql")
+  ).toString("utf-8");
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  /**
+   * For production environments, use
+   * ApolloServerPluginLandingPageProductionDefault instead.
+   **/
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+});
+
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+
